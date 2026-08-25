19330288dbffc032be23618bbbc3e71426518345
the diffigral <prankrishna001@gmail.com>
2026-08-25 12:14:01 +0530

    Create site-config.js

---
+++ site-config.js
@@ -0,0 +1,19 @@
+const SITE_CONFIG = {
+
+  books: [
+    "Days at the Morisaki Bookshop - Satoshi Yagisawa",
+    "Gene Machine - Venki Ramakrishnan",
+    "Heart Lamp - Banu Mushtaq",
+    "Sister of My Heart - Chitra Banerjee",
+    "The Forest of Enchantments - Chitra Banerjee",
+    "The Other Side of Silence - Urvashi Butalia",
+    "The Village By The Sea - Anita Desai"
+  ],
+
+  movies: [
+    "Musafir cafe",
+    "Thappad",
+    "The Odyssey"
+  ]
+
+};
